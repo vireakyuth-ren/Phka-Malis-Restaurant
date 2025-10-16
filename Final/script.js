af0bd6dbf2f4d7030214b4beaf35c82bfaa07e70
Vireakyuth REN <2024401ren@aupp.edu.kh>
2025-10-16 13:56:44 +0700

    Delete Final directory

--- Final/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-    const header = document.querySelector("header");
-    if (window.scrollY > 50) {
-        header.classList.add("shrink");
-    } else {
-        header.classList.remove("shrink");
-    }
-});
