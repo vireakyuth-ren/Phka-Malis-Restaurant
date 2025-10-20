fc6200538e6d0d578cc02f089ab44846945c6cb0
Vireakyuth REN <2024401ren@aupp.edu.kh>
2025-10-20 10:44:16 +0700

    Update script.js
    make footer animation stop at final moment

--- script.js
+++ script.js
@@ -5,4 +5,33 @@ window.addEventListener("scroll", function() {
     } else {
         header.classList.remove("shrink");
     }
+
+});
+
+window.addEventListener("scroll", function() {
+    const footerImages = document.querySelectorAll(".footer-image img");
+    const footerContents = document.querySelector(".footer-contents");
+    const footerSection = document.querySelector(".footer");
+    
+    // Get the footer position
+    const footerTop = footerSection.offsetTop;
+    const windowHeight = window.innerHeight;
+    const scrollY = window.scrollY;
+    
+    // Check if footer is in view
+    if (scrollY + windowHeight >= footerTop) {
+        // Disable AOS animations for footer elements
+        footerImages.forEach(img => {
+            img.setAttribute("data-aos", "");
+        });
+        footerContents.setAttribute("data-aos", "");
+        
+        // Make animations static (remove the data-aos attributes that trigger animations)
+        footerImages.forEach(img => {
+            img.style.opacity = "1";
+            img.style.transform = "none";
+        });
+        footerContents.style.opacity = "1";
+        footerContents.style.transform = "none";
+    }
 });
